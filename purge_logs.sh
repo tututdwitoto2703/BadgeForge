99b6083bf4dc355e70ff6ecf79c285531b71cf9b
sol4nki <solankipranjal44@gmail.com>
2025-12-21 18:34:07 +0530

    purge logs bash script

---
+++ purge_logs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\033[33m'
+NC='\e[0m'
+echo ""
+printf "${RED}Are you sure you want to purge the logs?${NC}\n"
+echo ""
+echo "We are currently in the directory:"
+printf "========${YELLOW}\n"
+pwd
+printf "${NC}========\n"
+echo ""
+echo "Pls enter y/n"
+read choice
+
+if [[ "$choice" == "y" || "$choice" == "Y" ]]
+then
+    echo ""
+    printf "${RED}Purging Logs [\]\n${NC}"
+    echo ""
+    rm -rf ./logs
+    mkdir ./logs
+    printf "${GREEN}Purged Successfully [!]\n"
+    printf "Exiting...${NC}\n"
+else
+    printf "${GREEN}Not Purging the logs\n"
+    printf "Exiting...\n${NC}"
+fi
+
+# rm -rf ./logs
